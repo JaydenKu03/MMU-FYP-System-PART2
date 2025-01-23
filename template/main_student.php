c520989fd6575473b8d1b2f1f74b52f199613258
JaydenKuMingHin <1211311715@student.mmu.edu.my>
2025-01-24 03:12:37 +0800

    update

---
+++ template/main_student.php
@@ -0,0 +1,23 @@
+<div class="operation-task">
+    <a href="approved_list.php">
+        <img src="images/approved.png" alt="approved_list">
+        <p>Approved Project</p>
+    </a>
+</div>
+<div class="operation-task">
+    <a href="meeting.php">
+        <img src="images/submit_meeting.png" alt="pending_list">
+        <p>Meeting Management</p>
+    </a>
+</div>
+<div class="operation-task">
+    <a href="goals.php">
+        <img src="images/goal_and_progress.png" alt="student_list">
+        <p>Goal and Progress</p>
+    </a>
+</div>
+<div class="operation-task">
+    <a href="proposal_submission.php"><img src="images/submit_proposal.png" alt="registration">
+        <p>Proposal Submission</p>
+    </a>
+</div>
---
+++ template/main_supervisor.php
@@ -0,0 +1,36 @@
+<div class="operation-task">
+    <a href="approved_list.php">
+        <img src="images/approved.png" alt="approved_list">
+        <p>Approved Project</p>
+    </a>
+</div>
+<div class="operation-task">
+    <a href="supervised.php">
+        <img src="images/supervised.png" alt="pending_list">
+        <p>Supervised Student</p>
+    </a>
+</div>
+<div class="operation-task">
+    <a href="meeting_sv.php">
+        <img src="images/submit_meeting.png" alt="pending_list">
+        <p>Meeting Management</p>
+    </a>
+</div>
+<div class="operation-task">
+    <a href="goals_sv.php">
+        <img src="images/goal_and_progress.png" alt="student_list">
+        <p>Goal and Progress</p>
+    </a>
+</div>
+<div class="operation-task">
+    <a href="proposal_submission.php">
+        <img src="images/submit_proposal.png" alt="registration">
+        <p>Proposal Submission</p>
+    </a>
+</div>
+<div class="operation-task">
+    <a href="assessment.php">
+        <img src="images/submit_assesstment.png" alt="registration">
+        <p>Assesstment Submission</p>
+    </a>
+</div>
